d1f432b59d73fe1f26411cfc782110a7ea4aafa0
Joyanta Mondal <engr.joyantamondal@gmail.com>
2022-07-01 17:07:45 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const app = express();
+const port = 5000;
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+const uri = "mongodb://pherotask:MHgzkkg3BXbUffjV@cluster0-shard-00-00.bdgqc.mongodb.net:27017,cluster0-shard-00-01.bdgqc.mongodb.net:27017,cluster0-shard-00-02.bdgqc.mongodb.net:27017/?ssl=true&replicaSet=atlas-1ib0bq-shard-0&authSource=admin&retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db('jobtask');
+    const addBillingCollection = database.collection('add-billing');
+    
+    // // POST API 
+    app.post('/add-billing', async (req,res)=>{
+      const billinResult = req.body;
+
+      console.log('Hit The Service Api', billinResult);
+      const result = await addBillingCollection.insertOne(billinResult);
+   
+     return res.json(result)
+    })
+       // GET API
+       app.get('/billing-list',async(req,res)=>{
+        const cursor = addBillingCollection.find({});
+        const billingLlists = await cursor.toArray();
+        res.send(billingLlists);
+    });
+
+     // manage product delete 
+     app.delete('/update-billing/:id',async (req,res)=>{
+      const result = await addBillingCollection.deleteOne({_id: ObjectId(req.params.id)
+      });
+      res.send(result);
+  })
+  
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.listen(port,()=>{
+console.log('Listeting to port', port)
+})
